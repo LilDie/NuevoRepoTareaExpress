2f6a407286c0ecd1626f00a014b126620370fbb4
LilDie <neobad.reyes@gmail.com>
2024-02-19 22:56:40 -0300

    Primer commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const dbconnect = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/Api-Users');
+        console.log('Conexión exitosa a la base de datos');
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error.message);
+    }
+};
+
+module.exports = dbconnect;
